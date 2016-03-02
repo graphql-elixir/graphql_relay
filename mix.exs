246c54f133f74e0d7e630a4762cd1fd67ed1a97b
Sean Abrahams <abrahams@gmail.com>
2016-03-02 12:11:51 -0800

    Version bump for publishing to hex.

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule GraphQL.Relay.Mixfile do
   use Mix.Project
 
-  @version "0.0.5"
+  @version "0.0.6"
   @description "Elixir implementation of Relay for GraphQL"
   @repo_url "https://github.com/seanabrahams/graphql-relay-elixir"
 
